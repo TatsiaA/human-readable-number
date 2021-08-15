f0612363de6f8efca3af5df84d316c1ddfae8bd3
TatsiaA <taniokk@mail.ru>
2021-08-16 01:52:47 +0300

    fix: change equals

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function toReadable (num) {
  
-   let firstNum = ['1', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+   let firstNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
    let secondNum = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
    let thirdNum = ['3', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
@@ -11,21 +11,21 @@ let result;
         } else if (number.length == 2) {
             if (10 < number < 20) {
                 result = `${secondNum[number-11]}`;
-            } else if (number[1] === 0) {
-                result = `${thirdNum[number/10]}`;
+            } else if (number[1] == 0) {
+                result = `${thirdNum[number[0]]}`;
                    } else {
                    result = `${thirdNum[number[0]]} ${firstNum[number[1]]}`;
                 }
         } else if (number.length == 3) {
-            if ((number[1] == 1) && (number[2] === 0)) {
+            if ((number[1] == 1) && (number[2] == 0)) {
                 result = `${firstNum[number[0]]} hundred ten`;
             } else if ((number[1]) == 1) {
                 result = `${firstNum[number[0]]} hundred ${secondNum[((number[2])-1)]}`;
-            } else if ((number[1]) === 0 && (number[2]) === 0) {
+            } else if ((number[1]) == 0 && (number[2]) == 0) {
                 result = `${firstNum[number[0]]} hundred`;
-            } else if ((number[1]) === 0) {
+            } else if ((number[1]) == 0) {
                 result = `${firstNum[number[0]]} hundred ${firstNum[number[2]]}`;
-            } else if ((number[2]) === 0) {
+            } else if ((number[2]) == 0) {
                 result = `${firstNum[number[0]]} hundred ${thirdNum[number[1]]}`;
             } else {
                 result = `${firstNum[number[0]]} hundred ${thirdNum[number[1]]} ${firstNum[number[2]]}`;
