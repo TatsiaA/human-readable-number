75939b4086554b56bcc7595b4fd1058a84a2f0c7
Tatsiana <85485184+TatsiaA@users.noreply.github.com>
2023-01-28 01:03:50 +0300

    refactor: correct solution

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
 module.exports = function toReadable (num) {
- 
+
    let firstNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
    let secondNum = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
    let thirdNum = ['hundred', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
    let number = String(num);
-   let result;
         if (number.length == 1) {
-            result = `${firstNum[number]}`;
-        } else if (number.length == 2) {
+            return `${firstNum[number]}`;
+        }
+        if (number.length === 2) {
             if (number > 10 && number < 20) {
-                result = `${secondNum[number-11]}`;
-            } else if ((number[1]) == 0) {
-                result = `${thirdNum[number[0]]}`;
-                   } else {
-                   result = `${thirdNum[number[0]]} ${firstNum[number[1]]}`;
-                }
+                return `${secondNum[number-11]}`;
+            }
+            if ((number[1]) == 0) {
+                return `${thirdNum[number[0]]}`;
+            } else {
+                return `${thirdNum[number[0]]} ${firstNum[number[1]]}`;
+            }
         } else if (number.length == 3) {
             if ((number[1] == 1) && (number[2] == 0)) {
-                result = `${firstNum[number[0]]} hundred ten`;
-            } else if ((number[1]) == 1) {
-                result = `${firstNum[number[0]]} hundred ${secondNum[((number[2])-1)]}`;
-            } else if ((number[1]) == 0 && (number[2]) == 0) {
-                result = `${firstNum[number[0]]} hundred`;
-            } else if ((number[1]) == 0) {
-                result = `${firstNum[number[0]]} hundred ${firstNum[number[2]]}`;
-            } else if ((number[2]) == 0) {
-                result = `${firstNum[number[0]]} hundred ${thirdNum[number[1]]}`;
-            } else {
-                result = `${firstNum[number[0]]} hundred ${thirdNum[number[1]]} ${firstNum[number[2]]}`;
+                return `${firstNum[number[0]]} hundred ten`;
             }
+            if ((number[1]) == 1) {
+                return `${firstNum[number[0]]} hundred ${secondNum[((number[2])-1)]}`;
+            }
+            if ((number[1]) == 0 && (number[2]) == 0) {
+                return `${firstNum[number[0]]} hundred`;
+            }
+            if ((number[1]) == 0) {
+                return `${firstNum[number[0]]} hundred ${firstNum[number[2]]}`;
+            }
+            if ((number[2]) == 0) {
+                return `${firstNum[number[0]]} hundred ${thirdNum[number[1]]}`;
+            }
+            return `${firstNum[number[0]]} hundred ${thirdNum[number[1]]} ${firstNum[number[2]]}`;
         } else {
             return "Incorrect number";
         }
-        return result;
     };
-
-
